577b37f2b4bd5c0ce3041aa486ba00f91e497b12
wangxinleo <wangxin.leo@outlook.com>
2020-09-23 08:40:37 +0800

    项目初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,100 @@
+module.exports = {
+  base: '/',
+  title: 'OA-DevHelpDoc',
+  description: '五株科技BPM开发帮助文档',
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+      title: 'OA-DevHelpDoc',
+      description: '五株科技BPM开发帮助文档',
+    },
+    '/en/': {
+      lang: 'en-US',
+      title: 'OA-DevHelpDoc',
+      description: 'WuZhu Technology BPM Development Assistance Documents',
+    },
+  },
+  themeConfig: {
+    logo: '/img/logo.png',
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '概述', link: '/summary/' },
+      { text: 'Github', link: 'https://google.com', target: '_blank' },
+    ],
+    locales: {
+      '/': {
+        selectText: '选择语言',
+        label: '简体中文',
+        editLinkText: '在 GitHub 上编辑此页',
+        lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+        serviceWorker: {
+          updatePopup: {
+            message: '发现新内容可用.',
+            buttonText: '刷新',
+          },
+        },
+        nav: [
+          { text: '主页', link: '/' },
+          { text: 'BPM系统', link: '/summary/' },
+          { text: 'Github', link: 'https://google.com', target: '_blank' },
+        ],
+        sidebar: {
+          '/summary/': [
+            {
+              title: '概述',
+              // false 为默认展开
+              collapsable: false,
+              children: [
+                { title: '概述', path: '/summary/' },
+                { title: '项目简介', path: '/guide/project' },
+              ],
+            },
+            {
+              title: '深入',
+              // false 为默认展开
+              collapsable: false,
+              children: [
+                { title: '概述', path: '/summary/' },
+                { title: '项目简介', path: '/guide/project' },
+              ],
+            },
+          ],
+          '/nested/': [''],
+        },
+      },
+      '/en/': {
+        selectText: 'Languages',
+        label: 'English',
+        editLinkText: 'Edit this page on GitHub',
+        lang: 'en-US',
+        serviceWorker: {
+          updatePopup: {
+            message: 'New content is available.',
+            buttonText: 'Refresh',
+          },
+        },
+        nav: [
+          { text: 'Home', link: '/en/' },
+          { text: 'Guide', link: '/en/guide/' },
+          { text: 'Github', link: 'https://google.com', target: '_blank' },
+        ],
+        // 当前 locale 的 algolia docsearch 选项
+        // algolia: {},
+        // nav: [{ text: 'Nested', link: '/nested/' }],
+        // sidebar: {
+        //   '/en/': [''],
+        //   '/en/nested/': [''],
+        // },
+      },
+    },
+  },
+  plugins: [
+    '@vuepress/active-header-links',
+    {
+      sidebarLinkSelector: '.sidebar-link',
+      headerAnchorSelector: '.header-anchor',
+    },
+  ],
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /img/logo.png
+heroText: OA-DevHelpDoc
+tagline: BPM系统开发帮助文档
+actionText: 查看详情 →
+actionLink: /guide/
+features:
+  - title: 兼容Trident及V8引擎
+    details: 以兼容 IE8 为中心的JavaScript代码实例，以ES4.1为主，配合ES6新特性开发。
+  - title: 两年开发实例驱动
+    details: 兼顾解决IE浏览器和Chrome浏览器部分代码不兼容的问题,两年代码实例沉淀，代码段快速定位。
+  - title: 成熟开发框架开箱即用
+    details: 一套Vue + Webpack 开发框架，享受快速开发体验，全方位适应企业的后台管理框架。
+footer: MIT Licensed | Copyright © 2020-wangxinleo
+---
